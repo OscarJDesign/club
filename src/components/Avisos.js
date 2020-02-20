32f4e7a16c4efce5017962ed45d3cdb8d678e792
OscarJDesign <oscarj.larenas.nova@gmail.com>
2020-02-20 17:53:24 -0300

    avisos responsivo

--- src/components/Avisos.js
+++ src/components/Avisos.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
+import chica from "../asset/img/chica.jpg";
 
 
-const ContenedorPublicidad = styled.div`
+
+const Contenedoravisos = styled.div`
     section{
         background:rgba(255, 255, 255, .6);
         margin:0px 20px 20px 20px;
@@ -26,74 +28,115 @@ const ContenedorPublicidad = styled.div`
             display: flex;
             flex-direction:row;
             flex-wrap:wrap;
-            justify-content:space-between;
+            
             padding:20px;
 
             .cajaimagenes{
-                display:flex;
-                justify-content:flex-end;
                 width:100%;
                 padding:10px;
                 align-items:center;
                 text-align:center;
                 border-radius:8px;
-                padding-bottom:40px;
                 
                 .imagen{
                     display:flex;
-                    width:25%;
+                    justify-content:flex-start;
+                    width:100%;
                     margin-bottom:-35px;
                     border-radius:4px;
-                    z-index:50;
+                   
                     
                     img{
-                        width:100%;
+                        width:20%;
                         
                         box-shadow: 5px 5px 10px rgba(0, 0, 0, .6);
                         vertical-align:top;
-                        border-radius:4px;    
+                        border-radius:4px; 
+                        z-index: 50;
+                    
                     }
                 }   
                 .datos-chica{
+                    padding:30px 0px 30px 160px;
+                    
                     display:inline-block;
-                    width:100%;
+                    position:relative;
+                    top:-150px;
+                    width:80%;
                     justify-content:center;
-                    background:rgba(0, 0, 0, .6);
+                    background:rgba(255,192,203 ,.4 );
                     border-radius:4px;
                     box-shadow: 5px 5px 10px rgba(0, 0, 0, .6);
-                    margin-left:-60px;  
-                
                     
                     .tituloaviso{
-                        margin:20px 40px;
-                        color:#D4AFB9;
-                        font-size:30px;
+                        color:rgb(255, 17, 108);
                         text-decoration:none;
                         :hover{
-                            color:rgb(255, 17, 108);
+                            color:rgba(0, 0, 0, .6);
                         }
-                    }
-                    .descripcionAviso{
-                        padding:0px 70px;
-                        color:white;
+                        }
+                    
+                    p{
+                        padding-top:25px;
+                        margin: 0px;
+                        color:black;
                     }
                 }
             }
-        }   
+            
+        }
     }
-   
-} 
-@media screen and (max-width: 560px){
-  section .contenedor .efecto-titulo{
-      
-  }
+    
 }
-   
+    @media screen and (max-width: 1100px){
+        section .contenedor .efecto-titulo{
+            font-size:35px;
+            color:rgb(255, 17, 108);
+        }
+        section .contenedor .contenedor-imagenes .cajaimagenes{
+            .imagen {
+                justify-content:center;
+                img{
+                width:25%;
+                }   
+            }
+            .datos-chica{
+                padding:50px 0px 50px 20px;
+                padding-bottom:;
+                position: relative;
+                padding-left:0px;
+                width:100%;
+                top:0px;
+                
+            }
+            
+        }
+        
+    }
+    @media screen and (max-width:800px){
+        section .contenedor .efecto-titulo{
+            font-size:30px;
+            color:rgb(255, 17, 108);
+        }
+        section .contenedor .contenedor-imagenes .cajaimagenes{
+            .imagen img{
+                width:37%;
+            }
+        }
+    }
+    @media screen and (max-width: 500px){
+        section .contenedor .contenedor-imagenes .cajaimagenes{
+           .imagen img{
+               width:73%;
+           }
+        }
+    }
 `;
+
 const Avisos = () => {
     return (
-        <ContenedorPublicidad>
-            <section className="seccion">
+        <Contenedoravisos>
+          <section className="SeccionChicas">
                 <div className="contenedor">
                     <h2 className="efecto-titulo">Avisos</h2>
                     <div className="contenedor-imagenes"> 
@@ -102,26 +145,29 @@ const Avisos = () => {
                                 <img src="http://18.217.42.238/uploads/d4e07ff5-d7fa-4b09-86b9-f4cdbb4d838a.jpeg"  width = "" height="" alt="Logo_ClubVip"/>
                             </div>
                             <div className="datos-chica">
-                                    
-                                    <a href="#!" className="tituloaviso">TITULO DEL AVISO</a>
-                                    <p className="descripcionAviso"> Descripcion del aviso </p>
+                                <a href="#!" className="tituloaviso">HERMOSAS CHICAS FULL NOCHE</a>
+                                <p className="descripcionAviso">ESTA NOCHE TE ESPERAN HERMOSAS ESCORT EN CONCEPCION CENTRO, PARA QUE DISFRUTES Y TERELAJES CON NOSOTRAS ... </p>
                             </div>
                         </div>
                         <div className="cajaimagenes">
                             <div className="imagen">
-                                <img src="http://18.217.42.238/uploads/d4e07ff5-d7fa-4b09-86b9-f4cdbb4d838a.jpeg"  width = "" height="" alt="Logo_ClubVip"/>
+                                <img  src="http://18.217.42.238/uploads/d4e07ff5-d7fa-4b09-86b9-f4cdbb4d838a.jpeg"  width = "" height="" alt="Logo_ClubVip"/>
                             </div>
                             <div className="datos-chica">
-                                    
-                                    <a href="#!" className="tituloaviso">HERMOSAS CHICAS FULL NOCHE</a>
-                                    <p className="descripcionAviso">ESTA NOCHE TE ESPERAN HERMOSAS ESCORT EN CONCEPCION CENTRO, PARA QUE DISFRUTES Y TE RELAJES CON NOSOTRAS ... </p>
+                                <a href="#!" className="tituloaviso">HERMOSAS CHICAS FULL NOCHE</a>
+                                <p className="descripcionAviso">Esta noche te esperan hermosas escort en concepción centro, para que disfrutes y te relajes con nosotras ... </p>
                             </div>
                         </div>
-                    </div> 
-                </div>           
-            </section>
-        </ContenedorPublicidad>
+                        
+                        
+                    </div>      
+                </div>
+            </section>  
+        </Contenedoravisos>
     );
-};
+};    
+
+
+
 
 export default Avisos;
--- src/components/Chicasagencia.js
+++ src/components/Chicasagencia.js
@@ -46,7 +46,7 @@ const Contenedorchicas = styled.div`
                     
                     img{
                         width:80%;
-                        height:330px ;
+                        
                         box-shadow: 5px 5px 10px rgba(0, 0, 0, .6);
                         vertical-align:top;
                         border-radius:4px; 
