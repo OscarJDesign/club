85160a90e6cf3bde1fb49a2193f0a244d51fd804
Matías <m.troncoso@saicoma.cl>
2020-02-26 15:31:24 -0300

    Pasando datos de la API

--- src/components/Chicasagencia.js
+++ src/components/Chicasagencia.js
@@ -1,233 +1,153 @@
-import React from 'react';
+import React from "react";
 import styled from "@emotion/styled";
 import wsp from "../asset/img/wsp.png";
-import chica from "../asset/img/chica.png"
-
+// import chica from "../asset/img/chica.png"
+import axios from "axios";
 
 const Contenedorchicas = styled.div`
-    section{
-        background:rgba(255, 255, 255, .6);
-        margin:0px 20px 20px 20px;
-        padding: 10px;
-        border-radius:10px;
-        box-shadow: 10px 10px 7px rgba(0, 0, 0, .6);
-        .contenedor{
-        overflow:hidden;
-        align-items:center;
-        
-        
-        .efecto-titulo{
-            margin-top:0px;
-            font-size:45px;
-            color:rgb(255, 17, 108);
-            text-align:center;
-            text-shadow: 3px 4px 4px rgb(0, 0, 0);
-            }
+  section {
+    background: rgba(255, 255, 255, 0.6);
+    margin: 0px 20px 20px 20px;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 10px 10px 7px rgba(0, 0, 0, 0.6);
+    .contenedor {
+      overflow: hidden;
+      align-items: center;
 
-        .contenedor-imagenes{
+      .efecto-titulo {
+        margin-top: 0px;
+        font-size: 45px;
+        color: rgb(255, 17, 108);
+        text-align: center;
+        text-shadow: 3px 4px 4px rgb(0, 0, 0);
+      }
+
+      .contenedor-imagenes {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        padding: 20px;
+
+        .cajaimagenes {
+          width: 25%;
+          padding: 10px;
+          align-items: center;
+          text-align: center;
+          border-radius: 8px;
+
+          .imagen {
             display: flex;
-            flex-direction:row;
-            flex-wrap:wrap;
-            justify-content:space-between;
-            padding:20px;
+            justify-content: center;
+            width: 100%;
+            margin-bottom: -35px;
+            border-radius: 4px;
 
-            .cajaimagenes{
-                width:25%;
-                padding:10px;
-                align-items:center;
-                text-align:center;
-                border-radius:8px;
-                
-                .imagen{
-                    display:flex;
-                    justify-content:center;
-                    width:100%;
-                    margin-bottom:-35px;
-                    border-radius:4px;
-                    
-                    img{
-                        width:80%;
-                        
-                        box-shadow: 5px 5px 10px rgba(0, 0, 0, .6);
-                        vertical-align:top;
-                        border-radius:4px; 
-                    
-                    }
-                }   
-                .datos-chica{
-                    display:inline-block;
-                    width:100%;
-                    justify-content:center;
-                    background:rgba(0, 0, 0, .6);
-                    border-radius:4px;
-                    box-shadow: 5px 5px 10px rgba(0, 0, 0, .6);
-                    .icono-wsp{
-                        display:flex;
-                        justify-content:center;
-                        cursor:pointer;
-                        padding:5px 0px;
-                        img{
+            img {
+              width: 80%;
 
-                        }
-                    }
-                    p{
-                        margin: 0px;
-                        color:white;
-                    }
-                }
+              box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.6);
+              vertical-align: top;
+              border-radius: 4px;
             }
-            
+          }
+          .datos-chica {
+            display: inline-block;
+            width: 100%;
+            justify-content: center;
+            background: rgba(0, 0, 0, 0.6);
+            border-radius: 4px;
+            box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.6);
+            .icono-wsp {
+              display: flex;
+              justify-content: center;
+              cursor: pointer;
+              padding: 5px 0px;
+              img {
+              }
+            }
+            p {
+              margin: 0px;
+              color: white;
+            }
+          }
         }
+      }
     }
-    
-}
-    @media screen and (max-width: 1100px){
-        section .contenedor .efecto-titulo{
-            font-size:35px;
-            color:rgb(255, 17, 108);
-        }
-        section .contenedor .contenedor-imagenes .cajaimagenes{
-            width:33.3%;
-        }
-        
+  }
+  @media screen and (max-width: 1100px) {
+    section .contenedor .efecto-titulo {
+      font-size: 35px;
+      color: rgb(255, 17, 108);
     }
-    @media screen and (max-width:800px){
-        section .contenedor .efecto-titulo{
-            font-size:30px;
-            color:rgb(255, 17, 108);
-        }
-        section .contenedor .contenedor-imagenes .cajaimagenes{
-            width:50%;
-        }
+    section .contenedor .contenedor-imagenes .cajaimagenes {
+      width: 33.3%;
     }
-    @media screen and (max-width: 500px){
-        section .contenedor .contenedor-imagenes .cajaimagenes{
-            width:100%;
-        }
+  }
+  @media screen and (max-width: 800px) {
+    section .contenedor .efecto-titulo {
+      font-size: 30px;
+      color: rgb(255, 17, 108);
     }
+    section .contenedor .contenedor-imagenes .cajaimagenes {
+      width: 50%;
+    }
+  }
+  @media screen and (max-width: 500px) {
+    section .contenedor .contenedor-imagenes .cajaimagenes {
+      width: 100%;
+    }
+  }
 `;
 
-const Chicasagencia = () => {
+export default class Chicasagencia extends React.Component {
+  state = {
+    listaImagenes: []
+  };
+
+  componentDidMount() {
+    axios.get(`http://18.217.42.238/api/chicasAgencia`).then(res => {
+      const listaImagenes = res.data;
+      console.log(res.data[0].Imagenes[0].path)
+      this.setState({ listaImagenes });
+    });
+  }
+
+  render() {
     return (
-        <Contenedorchicas>
-          <section className="SeccionChicas">
-                <div className="contenedor">
-                    <h2 className="efecto-titulo">Chicas de la Agencia</h2>
-                    <div className="contenedor-imagenes"> 
-                        <div className="cajaimagenes">
-                            <div className="imagen">
-                                <img src={chica}  width = "" height="" alt="Logo_ClubVip"/>
-                            </div>
-                            <div className="datos-chica">
-                                    <a href="#!" className="icono-wsp" >
-                                        <img src={wsp}  width = "" height="" alt="wsp"/>
-                                    </a> 
-                                    <p className="nombre-modelo">Nombre</p>
-                                    <p className="descripcion">Nacionalidad, Edad</p>
-                                    <p className="medidas">Medidas</p>
-                            </div>
-                        </div>
-                        <div className="cajaimagenes">
-                            <div className="imagen">
-                                <img src={chica}  width = "" height="" alt="Logo_ClubVip"/>
-                            </div>
-                            <div className="datos-chica">
-                                    <a href="#!" className="icono-wsp" >
-                                        <img src={wsp}  width = "" height="" alt="wsp"/>
-                                    </a> 
-                                    <p className="nombre-modelo">Nombre</p>
-                                    <p className="descripcion">Nacionalidad, Edad</p>
-                                    <p className="medidas">Medidas</p>
-                            </div>
-                        </div>
-                        <div className="cajaimagenes">
-                            <div className="imagen">
-                                <img src={chica}  width = "" height="" alt="Logo_ClubVip"/>
-                            </div>
-                            <div className="datos-chica">
-                                    <a href="#!" className="icono-wsp" >
-                                        <img src={wsp}  width = "" height="" alt="wsp"/>
-                                    </a> 
-                                    <p className="nombre-modelo">Nombre</p>
-                                    <p className="descripcion">Nacionalidad, Edad</p>
-                                    <p className="medidas">Medidas</p>
-                            </div>
-                        </div>
-                        <div className="cajaimagenes">
-                            <div className="imagen">
-                                <img src={chica}  width = "" height="" alt="Logo_ClubVip"/>
-                            </div>
-                            <div className="datos-chica">
-                                    <a href="#!" className="icono-wsp" >
-                                        <img src={wsp}  width = "" height="" alt="wsp"/>
-                                    </a> 
-                                    <p className="nombre-modelo">Nombre</p>
-                                    <p className="descripcion">Nacionalidad, Edad</p>
-                                    <p className="medidas">Medidas</p>
-                            </div>
-                        </div>
-                        <div className="cajaimagenes">
-                            <div className="imagen">
-                                <img src={chica}  width = "" height="" alt="Logo_ClubVip"/>
-                            </div>
-                            <div className="datos-chica">
-                                    <a href="#!" className="icono-wsp" >
-                                        <img src={wsp}  width = "" height="" alt="wsp"/>
-                                    </a> 
-                                    <p className="nombre-modelo">Nombre</p>
-                                    <p className="descripcion">Nacionalidad, Edad</p>
-                                    <p className="medidas">Medidas</p>
-                            </div>
-                        </div>
-                        <div className="cajaimagenes">
-                            <div className="imagen">
-                                <img src={chica}  width = "" height="" alt="Logo_ClubVip"/>
-                            </div>
-                            <div className="datos-chica">
-                                    <a href="#!" className="icono-wsp" >
-                                        <img src={wsp}  width = "" height="" alt="wsp"/>
-                                    </a> 
-                                    <p className="nombre-modelo">Nombre</p>
-                                    <p className="descripcion">Nacionalidad, Edad</p>
-                                    <p className="medidas">Medidas</p>
-                            </div>
-                        </div>
-                        <div className="cajaimagenes">
-                            <div className="imagen">
-                                <img src={chica}  width = "" height="" alt="Logo_ClubVip"/>
-                            </div>
-                            <div className="datos-chica">
-                                    <a href="#!" className="icono-wsp" >
-                                        <img src={wsp}  width = "" height="" alt="wsp"/>
-                                    </a> 
-                                    <p className="nombre-modelo">Nombre</p>
-                                    <p className="descripcion">Nacionalidad, Edad</p>
-                                    <p className="medidas">Medidas</p>
-                            </div>
-                        </div>
-                        <div className="cajaimagenes">
-                            <div className="imagen">
-                                <img src={chica}  width = "" height="" alt="Logo_ClubVip"/>
-                            </div>
-                            <div className="datos-chica">
-                                    <a href="#!" className="icono-wsp" >
-                                        <img src={wsp}  width = "" height="" alt="wsp"/>
-                                    </a> 
-                                    <p className="nombre-modelo">Nombre</p>
-                                    <p className="descripcion">Nacionalidad, Edad</p>
-                                    <p className="medidas">Medidas</p>
-                                    <p>dsadsadsa    </p>
-                            </div>
-                        </div>
-                        
-                    </div>      
+      <Contenedorchicas>
+        <section className="SeccionChicas">
+          <div className="contenedor">
+            <h2 className="efecto-titulo">Chicas de la Agencia</h2>
+            <div className="contenedor-imagenes">
+              {this.state.listaImagenes.map(imagenes => (
+                <div className="cajaimagenes">
+                  <div className="imagen">
+                    <img
+                      key={imagenes.Imagenes[0].id}
+                      src={"http://18.217.42.238/" + imagenes.Imagenes[0].path}
+                      width=""
+                      height=""
+                      alt="Logo_ClubVip"
+                    />
+                  </div>
+                  <div className="datos-chica">
+                    <a href="#!" className="icono-wsp">
+                      <img src={wsp} width="" height="" alt="wsp" />
+                    </a>
+                    <p className="nombre-modelo">{imagenes.nombre}</p>
+                    <p className="descripcion">{imagenes.nacionalidad}, {imagenes.edad}</p>
+              <p className="medidas">Medidas: {imagenes.busto} - {imagenes.cintura} - {imagenes.caderas}</p>
+                  </div>
                 </div>
-            </section>  
-        </Contenedorchicas>
+              ))}
+            </div>
+          </div>
+        </section>
+      </Contenedorchicas>
     );
-};    
+  }
+}
 
-
-
-
-export default Chicasagencia;
+// export default Chicasagencia;
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,171 +1,141 @@
-import React, {lazy, Suspense} from 'react';
+import React, { Suspense } from "react";
 import styled from "@emotion/styled";
 import Spinner from "./Spinner";
+import axios from "axios";
 
 // import video from "../asset/img/video.jpg";
 // import CuadroVideos from './CuadroVideos';
 
-const CuadroVideos = lazy(() => import("./CuadroVideos"));
+// const CuadroVideos = lazy(() => import("./CuadroVideos"));
 
 const ContenedorVideos = styled.div`
-
-section{
-    background:rgba(255, 255, 255, .6);
-    margin:130px 20px 0px 20px;
+  section {
+    background: rgba(255, 255, 255, 0.6);
+    margin: 130px 20px 0px 20px;
     padding: 3px;
-    border-radius:10px;
-    box-shadow: 10px 10px 7px rgba(0, 0, 0, .6);
-    
+    border-radius: 10px;
+    box-shadow: 10px 10px 7px rgba(0, 0, 0, 0.6);
 
-    .contenedor{
-        overflow:hidden;
-        align-items:center;
-        
-        
-        .efecto-titulo{
-            margin-top:0px;
-            font-size:45px;
-            color:rgb(255, 17, 108);
-            text-align:center;
-            text-shadow: 3px 4px 4px rgb(0, 0, 0);
-            }
+    .contenedor {
+      overflow: hidden;
+      align-items: center;
 
-        .contenedor-videos{
-            display: flex;
-            flex-direction:row;
-            flex-wrap:wrap;
-            justify-content:space-between;
+      .efecto-titulo {
+        margin-top: 0px;
+        font-size: 45px;
+        color: rgb(255, 17, 108);
+        text-align: center;
+        text-shadow: 3px 4px 4px rgb(0, 0, 0);
+      }
 
-            .cajavideos{
-                
-                padding:0px 10px;
-                width:33.3%;
-                align-items:center;
-                text-align:center;
-                
-                }   
-            }
-            
+      .contenedor-videos {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+
+        .cajavideos {
+          padding: 0px 10px;
+          width: 33.3%;
+          align-items: center;
+          text-align: center;
         }
+      }
     }
-    @media screen and (max-width: 1100px){
-        section .contenedor .efecto-titulo{
-            font-size:35px;
-            color:rgb(255, 17, 108);
-        }
-        section .contenedor .contenedor-videos .cajavideos{
-            width:50%;
-        }
-        
+  }
+  @media screen and (max-width: 1100px) {
+    section .contenedor .efecto-titulo {
+      font-size: 35px;
+      color: rgb(255, 17, 108);
     }
-    @media screen and (max-width:670px){
-        section .contenedor .contenedor-videos .cajavideos{
-            width:100%;
-            margin:0px;
-     }
+    section .contenedor .contenedor-videos .cajavideos {
+      width: 50%;
     }
-    @media screen and (max-width:500px){
-        section .contenedor .efecto-titulo{
-            font-size:30px;
-            color:rgb(255, 17, 108);
-        }
-        section .contenedor .contenedor-videos .cajavideos{
-            width:100%;
-            margin:0px;
-     }
+  }
+  @media screen and (max-width: 670px) {
+    section .contenedor .contenedor-videos .cajavideos {
+      width: 100%;
+      margin: 0px;
     }
-    
-    
+  }
+  @media screen and (max-width: 500px) {
+    section .contenedor .efecto-titulo {
+      font-size: 30px;
+      color: rgb(255, 17, 108);
+    }
+    section .contenedor .contenedor-videos .cajavideos {
+      width: 100%;
+      margin: 0px;
+    }
+  }
 `;
 
+const FotoVideo = styled.div`
+  width: 100%;
+  min-width: 300px;
+  margin-bottom: 20px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.6);
+  border-radius: 14px;
+  padding: 0px 0px;
+  video {
+    width: 100%;
 
-const Videos = () => {
+    vertical-align: top;
+    border-radius: 14px;
+    border: 1px solid rgba(0, 0, 0, 0.3);
+  }
+`;
+
+export default class Videos extends React.Component {
+  state = {
+    listaVideos: []
+  };
+
+  componentDidMount() {
+    axios.get(`http://18.217.42.238/api/videos`).then(res => {
+      const listaVideos = res.data;
+      this.setState({ listaVideos });
+    });
+  }
+
+  render() {
     return (
-        <ContenedorVideos id="videos" >
-           
-            <section className="SeccionVideos">
-                <div className="contenedor">
-                    <h2 className="efecto-titulo">Videos</h2>
-                    <div className="contenedor-videos"> 
-                    
-                        <div className="cajavideos">
-                            <Suspense fallback={
-                            <div>
-                                <Spinner />
-                            </div>
-                            }>
-                            <CuadroVideos/> 
-                            <div className="titulo-video">
-                                <p className="nombre-modelo">Informacion de la modelo</p>   
-                            </div>
-                            </Suspense> 
-                        </div>  
-                        <div className="cajavideos">
-                            <Suspense fallback={
-                            <div>
-                                <Spinner />
-                            </div>
-                            }>
-                            <CuadroVideos/> 
-                            <div className="titulo-video">
-                                <p className="nombre-modelo">Informacion de la modelo</p>   
-                            </div>
-                            </Suspense> 
-                        </div> 
-                        <div className="cajavideos">
-                            <Suspense fallback={
-                            <div>
-                                <Spinner />
-                            </div>
-                            }>
-                            <CuadroVideos/> 
-                            <div className="titulo-video">
-                                <p className="nombre-modelo">Informacion de la modelo</p>   
-                            </div>
-                            </Suspense> 
-                        </div>     
-                        <div className="cajavideos">
-                            <Suspense fallback={
-                            <div>
-                                <Spinner />
-                            </div>
-                            }>
-                            <CuadroVideos/> 
-                            <div className="titulo-video">
-                                <p className="nombre-modelo">Informacion de la modelo</p>   
-                            </div>
-                            </Suspense> 
-                        </div>   
-                        <div className="cajavideos">
-                            <Suspense fallback={
-                            <div>
-                                <Spinner />
-                            </div>
-                            }>
-                            <CuadroVideos/> 
-                            <div className="titulo-video">
-                                <p className="nombre-modelo">Informacion de la modelo</p>   
-                            </div>
-                            </Suspense> 
-                        </div>   
-                        <div className="cajavideos">
-                            <Suspense fallback={
-                            <div>
-                                <Spinner />
-                            </div>
-                            }>
-                            <CuadroVideos/> 
-                            <div className="titulo-video">
-                                <p className="nombre-modelo">Informacion de la modelo</p>   
-                            </div>
-                            </Suspense> 
-                        </div>         
-                    </div>
+      <ContenedorVideos id="videos">
+        <section className="SeccionVideos">
+          <div className="contenedor">
+            <h2 className="efecto-titulo">Videos</h2>
+            <div className="contenedor-videos">
+              {this.state.listaVideos.map(videos => (
+                <div className="cajavideos">
+                  <Suspense
+                    fallback={
+                      <div>
+                        <Spinner />
+                      </div>
+                    }
+                  >
+                    <FotoVideo className="foto-video">
+                      <video
+                        width="300"
+                        src={"http://18.217.42.238" + videos.path}
+                        controls
+                        controlsList="nodownload"
+                        disablePictureInPicture
+                        preload="metadata"
+                      >
+                        <source src="movie.mp4" type="video/mp4"></source>
+                      </video>
+                    </FotoVideo>
+                    {/* <div className="titulo-video">
+                  <p className="nombre-modelo">Informacion de la modelo</p>
+                </div> */}
+                  </Suspense>
                 </div>
-            </section>   
-        </ContenedorVideos>
-        
+              ))}
+            </div>
+          </div>
+        </section>
+      </ContenedorVideos>
     );
-};
-
-export default Videos;
+  }
+}
